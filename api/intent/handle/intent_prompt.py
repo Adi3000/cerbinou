0dcc98ca55a5d636c4fc02268706d228e77b950f
Adi3000 <adi300000@gmail.com>
2024-09-21 16:53:42 +0200

    fix(prompt): Wrong encoding when sending request

--- api/intent/handle/intent_prompt.py
+++ api/intent/handle/intent_prompt.py
@@ -50,7 +50,7 @@ json_template = {
 }
 
 prompt_context =  "Bonjour, es-tu disponible ?<|eot_id|><|start_header_id|>cerbinou<|end_header_id|>\n\nOui bien sûr, que veux tu savoir ?<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n"
-
+post_text_headers = {'Content-Type': 'text/plain; charset=utf-8'}
 
 def get_prompt_response(prompt: str):
     global prompt_context
@@ -95,8 +95,9 @@ def purge_context(context: str):
 def process_stream_reponse(response: requests.Response):
     text_response = "";
     if response.status_code == 200:
+        response.encoding = 'utf-8'
         sentence = "";
-        for line in response.iter_lines(decode_unicode=True, chunk_size=128):
+        for line in response.iter_lines(chunk_size=128):
             json_line = json.loads(line)
             if json_line["content"]:
                 sentence += json_line["content"]
@@ -107,15 +108,15 @@ def process_stream_reponse(response: requests.Response):
                 
 def flush_sentence(sentence: str):
     sentences_to_flush = re.split(stop_signs_regex, sentence)
-    
+    logger.info(f"phrase : {sentence}") 
     if len(sentences_to_flush) > 1:
         for i in range(len(sentences_to_flush) - 1):
             if sentences_to_flush[i]:
-                requests.post(f"{RHASSPY_URL}/api/text-to-speech", data=sentences_to_flush[i])
+                requests.post(f"{RHASSPY_URL}/api/text-to-speech", data=sentences_to_flush[i].encode("utf-8"), headers=post_text_headers)
         
     new_sentence = sentences_to_flush[-1]
     if any(symbol in new_sentence for symbol in stop_signs):
-        requests.post(f"{RHASSPY_URL}/api/text-to-speech", data=new_sentence)
+        requests.post(f"{RHASSPY_URL}/api/text-to-speech", data=new_sentence.encode("utf-8"), headers=post_text_headers)
         return ""
     else:
         return new_sentence
